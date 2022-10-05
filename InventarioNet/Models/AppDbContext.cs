f487a0433afe319d75515b1dfdf45bfa0ddd62e7
Nicolas pereira <90424448+senhorN@users.noreply.github.com>
2022-10-05 09:00:09 -0300

    intenal file

---
+++ InventarioNet/Models/AppDbContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace InventarioNet.Models
+{
+    public partial class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Produtos> Produtos { get; set; }
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=NICK\\SQLEXPRESS;Initial Catalog=InventarioDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produtos>(entity =>
+            {
+
+                //entity.Property(e => e.ProdutoId).ValueGeneratedNever();
+               
+
+                entity.HasKey(e => e.ProdutoId);
+
+                entity.Property(e => e.Imagem).HasMaxLength(250);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Preco).HasColumnType("decimal(18, 2)");
+            });
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.UsuarioId);
+
+                entity.Property(e => e.Email).HasMaxLength(250);
+
+                entity.Property(e => e.Login)
+                    .IsRequired()
+                    .HasMaxLength(80);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(80);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
